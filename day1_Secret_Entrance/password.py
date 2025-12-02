107ff6a7796a89bb55b76b65e1e9a589d9c5e4b2
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-12-02 11:42:56 +0100

    Add tests and functionality for left and right rotations equal to zero

--- day1_Secret_Entrance/password.py
+++ day1_Secret_Entrance/password.py
@@ -1,2 +1,15 @@
-def get_password(rotation):
-    return 0
+def get_password(rotation): # ['R50']
+    dial = 50
+    for r in rotation: # 'R50'
+        direction = r[0] # 'R'
+        rotate = int(r[1:]) # 50
+        if direction == 'L': # False
+            dial = dial - rotate # dial = 50 - 50
+        else:
+            dial = dial + rotate # dial = 50 + 50
+        if dial == 0: # False
+            return 1
+        elif dial == 100: # True (100)
+            return 1
+        else:
+            return 0
--- day1_Secret_Entrance/test_password.py
+++ day1_Secret_Entrance/test_password.py
@@ -6,4 +6,24 @@ def test_get_password_exists():
 
 
 def test_right_zero_():
-    assert get_password('R0') == 0
+    assert get_password(['R0']) == 0
+
+
+def test_left_zero_():
+    assert get_password(['L0']) == 0
+
+
+def test_right_one_():
+    assert get_password(['R1']) == 0
+
+
+def test_left_50():
+    assert get_password(['L50']) == 1
+
+
+def test_right_50():
+    assert get_password(['R50']) == 1
+
+
+def test_right_51():
+    assert get_password(['R50']) == 0
