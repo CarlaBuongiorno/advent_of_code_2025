1a0f8b384f7f3a24f3fb24a7d5c8f8045cd772b3
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-12-19 11:37:57 +0100

    Solve part two of day 3 with tests and sudo code

--- day3_Lobby/banks.py
+++ day3_Lobby/banks.py
@@ -1,17 +1,60 @@
+# def get_total_joltage(banks):
+#     results = []
+#     for bank in banks:
+#         sorted_bank = sorted(bank)
+#         highest = bank.index(sorted_bank[-1])
+#         if highest+1 < len(bank):
+#             sliced_bank = bank[highest+1:]
+#             sorted_sliced_bank = sorted(sliced_bank)
+#             second_highest = sliced_bank.index(sorted_sliced_bank[-1])
+#             results.append(f'{bank[highest]}{sliced_bank[second_highest]}')
+#         else:
+#             second_highest = bank.index(sorted_bank[-2])
+#             if highest < second_highest:
+#                 results.append(f'{bank[highest]}{bank[second_highest]}')
+#             else:
+#                 results.append(f'{bank[second_highest]}{bank[highest]}')
+#     return sum([int(number) for number in results])
+
+
+# -------------- part 2 -------------- #
+
+
+# def get_total_joltage(banks):
+#     results = []
+#     index_of_previous_digit = 0
+#     for bank in banks:
+#         for n in range(1,13):
+#             nth_digit, index_of_previous_digit = find_nth_digit(bank, n, index_of_previous_digit)
+#             results.append(nth_digit)
+
+        
+# def find_nth_digit(bank, n, index_of_previous_digit):
+#     current_range = bank[index_of_previous_digit:-(12-n)]
+#     highest_number = max(current_range)
+#     index_in_current_range = current_range.index(highest_number)
+#     index_in_bank = index_in_current_range+index_of_previous_digit
+#     return highest_number, index_in_bank
+
+
+
 def get_total_joltage(banks):
-    results = []
+    jolts = 0
     for bank in banks:
-        sorted_bank = sorted(bank)
-        highest = bank.index(sorted_bank[-1])
-        if highest+1 < len(bank):
-            sliced_bank = bank[highest+1:]
-            sorted_sliced_bank = sorted(sliced_bank)
-            second_highest = sliced_bank.index(sorted_sliced_bank[-1])
-            results.append(f'{bank[highest]}{sliced_bank[second_highest]}')
-        else:
-            second_highest = bank.index(sorted_bank[-2])
-            if highest < second_highest:
-                results.append(f'{bank[highest]}{bank[second_highest]}')
-            else:
-                results.append(f'{bank[second_highest]}{bank[highest]}')
-    return sum([int(number) for number in results])
+        results = []
+        index_of_previous_digit = 0
+        for n in range(1, 13):
+            nth_digit, index_of_previous_digit = find_nth_digit(bank, n, index_of_previous_digit)
+            results.append(str(nth_digit))
+        jolts += int(''.join(results))
+    return jolts
+
+
+def find_nth_digit(bank, n, index_of_previous_digit):
+    current_range = bank[index_of_previous_digit:len(bank)-(12-n)]
+    current_range_ints = [int(i) for i in current_range]
+    max_in_current_range = max(current_range_ints)
+    index_in_current_range = current_range.index(str(max_in_current_range))
+    index_in_bank = index_in_current_range+index_of_previous_digit
+    return max_in_current_range, index_in_bank+1
+
--- day3_Lobby/test_banks.py
+++ day3_Lobby/test_banks.py
@@ -5,24 +5,44 @@ def test_get_total_joltage_exists():
     assert get_total_joltage
 
 
-def test_get_two_largest_number_on_the_right():
-    assert get_total_joltage(['12']) == 12
+# def test_get_two_largest_number_on_the_right():
+#     assert get_total_joltage(['12']) == 12
 
 
-def test_get_largest_number_on_the_left():
-    assert get_total_joltage(['21']) == 21
+# def test_get_largest_number_on_the_left():
+#     assert get_total_joltage(['21']) == 21
 
 
-def test_get_two_largest_number_out_of_6():
-    assert get_total_joltage(['212435']) == 45
+# def test_get_two_largest_number_out_of_6():
+#     assert get_total_joltage(['212435']) == 45
 
 
-def test_get_two_largest_number_more_items():
-    assert get_total_joltage(['212534', '443647']) == 121
+# def test_get_two_largest_number_more_items():
+#     assert get_total_joltage(['212534', '443647']) == 121
 
 
-def test_get_one_example():
-    assert get_total_joltage(['818181911112111']) == 92
+# def test_get_one_example():
+#     assert get_total_joltage(['818181911112111']) == 92
+
+
+# def test_examples():
+#     assert get_total_joltage([
+#         '987654321111111',
+#         '811111111111119',
+#         '234234234234278',
+#         '818181911112111'
+#     ]) == 357
+
+
+# -------------- part 2 -------------- #
+
+
+def test_first_example():
+    assert get_total_joltage(['987654321111111']) == 987654321111
+
+
+def test_second_example():
+    assert get_total_joltage(['811111111111119']) == 811111111119
 
 
 def test_examples():
@@ -31,4 +51,26 @@ def test_examples():
         '811111111111119',
         '234234234234278',
         '818181911112111'
-    ]) == 357
+    ]) == 3121910778619
+
+
+'''
+To find the nth digit:
+    start from the index you found the previous digit
+    find the biggest number as long as there are still at least (12 - n) numbers left
+--------------------------------------------------------------------------------------
+To find the 1st digit:
+    start from the index you found the previous digit
+    find the biggest number as long as there are still at least 11 numbers left
+To find the 2nd digit:
+    start from the index you found the previous digit
+    find the biggest number as long as there are still at least 10 numbers left
+To find the 3rd digit:
+    start from the index you found the previous digit
+    find the biggest number as long as there are still at least 9 numbers left
+............
+To find the 12th digit:
+    start from the index you found the previous digit
+    find the biggest number as long as there are 0 numbers left
+
+'''
